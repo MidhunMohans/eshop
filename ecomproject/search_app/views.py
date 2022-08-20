38c868db693681d1df9d693587ba6cc3e879e89f
midhun <midhunmohan418@gmail.com>
2022-08-20 14:55:02 +0530

    added

---
+++ ecomproject/search_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from  shopapp.models import product
+from django.db.models import Q
+
+def searchResult(request):
+    products=None
+    query=None
+    if 'q' in request.GET:
+        query=request.GET.get('q')
+        products=product.objects.all().filter(Q(name__contains=query) | Q(desc__contains=query))
+        return render(request,'search.html',{'query':query,'products':products})
+
+
+
+
+# Create your views here.
